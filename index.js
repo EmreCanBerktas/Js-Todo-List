c092b80750733c8a78c7aa61b34d10c07356cbca
EmreCanBerktas <73836132+EmreCanBerktas@users.noreply.github.com>
2021-08-31 18:13:26 +0300

    to do list kısmı tamamlandı

---
+++ index.js
@@ -0,0 +1,42 @@
+var myNodeLıst = document.getElementsByTagName("li");
+for (let i = 0; i < myNodeLıst.length; i++) {
+  var span = document.createElement("span");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodeLıst[i].appendChild(span);
+}
+
+var close = document.getElementsByClassName("close");
+for (let i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+    var div = this.parentElement;
+    div.style.display = "none";
+  };
+}
+
+const newElement = () => {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("task").value;
+  var task = document.createTextNode(inputValue);
+  localStorage.setItem;
+  li.appendChild(task);
+  if (inputValue === "" || inputValue.replace(/^\s+|\s+$/g, "").length == 0) {
+    $(".error").toast("show");
+  } else {
+    $(".success").toast("show");
+    document.getElementById("list").appendChild(li);
+  }
+  document.getElementById("task").value = " ";
+  var span = document.createElement("span");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+  for (let i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      var div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
+};
